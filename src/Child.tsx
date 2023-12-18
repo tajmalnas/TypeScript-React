f42f4df9d264be0cb8fd39085a53315ecd773170
tajmalnas <tajjuddin.malnas21@pccoepune.org>
2023-12-18 19:00:58 +0530

    declaration of react functional component in typescript

--- src/Child.tsx
+++ src/Child.tsx
@@ -2,10 +2,16 @@ interface childProps {
     color: string;
 }
 
-const Child = ({color}:childProps) => {
-  return (
-    <div>{color}</div>
-  )
+// const Child = ({color}:childProps) => {
+//   return (
+//     <div>{color}</div>
+//   )
+// }
+
+const Child:React.FC<childProps> = ({color})=> {  //defining function component in  typescript as React.FC<props>
+    return (
+        <div>{color}</div>
+    )
 }
 
 export default Child
